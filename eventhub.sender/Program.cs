87f40b8124b0543eddf583a9428faf57858757a9
Sam Sorensen <825813+sabrsorensen@users.noreply.github.com>
2021-03-18 23:44:55 -0600

    Added template projects.

---
+++ eventhub.sender/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace eventhub.sender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
